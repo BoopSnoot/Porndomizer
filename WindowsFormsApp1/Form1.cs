7a5b808c358a7fcfd6ebd6f2df362cc4aa413ca1
Dmitry Grebenin <dminin2001@gmail.com>
2018-09-10 19:42:40 +0300

    v.69

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://pornhub.com");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random RandomMachine = new Random();
+            int Select = RandomMachine.Next(1, 15);
+            switch (Select)
+            {
+                case 1:
+                    Process.Start("https://xvideos.com");
+                    break;
+                case 2:
+                    Process.Start("https://fuq.com");
+                    break;
+                case 3:
+                    Process.Start("https://porn.com");
+                    break;
+                case 4:
+                    Process.Start("https://youporn.com");
+                    break;
+                case 5:
+                    Process.Start("https://xnxx.com");
+                    break;
+                case 6:
+                    Process.Start("https://xhamster.com");
+                    break;
+                case 7:
+                    Process.Start("https://hentaihaven.org");
+                    break;
+                case 8:
+                    Process.Start("https://youjizz.com");
+                    break;
+                case 9:
+                    Process.Start("https://youngpornvideos.com");
+                    break;
+                case 10:
+                    Form1 form = new Form1();
+                    form.label1.Visible = true;
+                    Process.Start("https://porzo.com");
+                    break;
+                case 11:
+                    Process.Start("https://porno365.com");
+                    break;
+                case 12:
+                    Process.Start("https://pornjam.com");
+                    break;
+                case 13:
+                    Process.Start("https://redtube.com");
+                    break;
+                case 14:
+                    Process.Start("https://pornhub.com");
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://pornhub.com");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://porno365.com");
+        }
+    }
+}
---
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Porndomizer")]
+[assembly: AssemblyDescription("Tired of old fucking porn websites, then this program is for you")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xxx_ABC_xxx")]
+[assembly: AssemblyProduct("Porndomizer")]
+[assembly: AssemblyCopyright("Fuck off")]
+[assembly: AssemblyTrademark("Fuck off")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("53d5f267-1db7-4cba-8ee6-7a8d73014aee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("69.69.69.69")]
+[assembly: AssemblyFileVersion("69.69.69.69")]
+[assembly: NeutralResourcesLanguage("ru")]
+
